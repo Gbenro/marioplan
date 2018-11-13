a43a78cc2f3d5c2c6498b787f9d1a2069b6be08c
Gbenro Adesoye <gbenro_adesoye@yahoo.com>
2018-11-12 20:47:21 -0600

    added Thunk for async

--- src/store/reducers/projectReducers.js
+++ src/store/reducers/projectReducers.js
@@ -1,7 +1,18 @@
 
 
-const initState ={}
+const initState ={
+
+    projects:[
+        {id:'1', title: 'help me find peach', content: 'blah blah blah'},
+        {id:'2', title: 'Hey, How can I help you', content: 'blah blah blah'},
+        {id:'3', title: 'What to do when?', content: 'blah blah blah'}
+    ]
+}
 const projectReducers = (state = initState, action)=>{
+    switch(action.type){
+        case 'CREATE_PROJECT':
+            console.log('created project', action.project)
+    }
 return state
 }
 
