041749f6e86d8eb6717abd8153a9cb0e14709b8d
Gbenro Adesoye <gbenro_adesoye@yahoo.com>
2018-12-06 17:32:28 -0600

    Updates

---
+++ precache-manifest.95aa0edbf6c81fb94737595098204955.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "03b7d155fa87cd8d69fa",
+    "url": "/static/js/main.03b7d155.chunk.js"
+  },
+  {
+    "revision": "90a324210a27090b731f",
+    "url": "/static/js/1.90a32421.chunk.js"
+  },
+  {
+    "revision": "03b7d155fa87cd8d69fa",
+    "url": "/static/css/main.e7a2af68.chunk.css"
+  },
+  {
+    "revision": "da6aadf4c2bd9967d809f102e9bdd403",
+    "url": "/index.html"
+  }
+];
