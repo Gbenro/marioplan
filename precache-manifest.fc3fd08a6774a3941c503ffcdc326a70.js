1d7307d1d86eb75577f0f841aa168abac96e9c24
Gbenro Adesoye <gbenro_adesoye@yahoo.com>
2018-12-06 17:50:31 -0600

    Updates

---
+++ precache-manifest.fc3fd08a6774a3941c503ffcdc326a70.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2d708f56a2c6dcc6802f",
+    "url": "/marioplan/static/js/runtime~main.2d708f56.js"
+  },
+  {
+    "revision": "a6a56c570758b02a31ae",
+    "url": "/marioplan/static/js/main.a6a56c57.chunk.js"
+  },
+  {
+    "revision": "6ae67e808238aa6750a8",
+    "url": "/marioplan/static/js/1.6ae67e80.chunk.js"
+  },
+  {
+    "revision": "a6a56c570758b02a31ae",
+    "url": "/marioplan/static/css/main.e7a2af68.chunk.css"
+  },
+  {
+    "revision": "e2b4603af0fd83cb111af70bf056a16c",
+    "url": "/marioplan/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2940dff5c785ffd5490ba1cdd3a87e98.js"
+  "/marioplan/precache-manifest.fc3fd08a6774a3941c503ffcdc326a70.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/marioplan/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
